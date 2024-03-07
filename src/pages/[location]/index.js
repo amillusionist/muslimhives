9dbd23b6b0d0e07571319bc4f805a4d1a6d922b2
Manish <manishwebworks@gmail.com>
2024-03-07 15:18:39 +0530

    path update

--- src/pages/[location]/index.js
+++ src/pages/[location]/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Poppins } from "next/font/google";
 import ListItemBox from "/components/listItemBox";
-import Breadcrumb from "/components/Breadcrumb";
+import Breadcrumb from "../../../components/Breadcrumb";
 
 
 const LocationPage = () => {
