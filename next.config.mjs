37dc03f4583f990e3c68bb15f5e1fcc03cbbb091
Manish <manishwebworks@gmail.com>
2024-03-07 16:01:46 +0530

    Fix 'output: export' configuration for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-          {
-              protocol: 'https',
-              hostname: '**',
-              port: '',
-              pathname: '**',
-          },
-      ],
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
